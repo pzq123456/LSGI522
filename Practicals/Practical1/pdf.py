8e6a4bd8bfde0d1c11e69edaffdc3375868108d8
潘志清 <82391775+pzq123456@users.noreply.github.com>
2024-10-15 21:26:53 +0800

    Update .gitignore and pdf.py for Practical1

--- Practicals/Practical1/pdf.py
+++ Practicals/Practical1/pdf.py
@@ -14,10 +14,11 @@ def merge_pdfs(pdf_list, output):
 if __name__ == "__main__":
 
     PARENT_DIR = "G:\\polyulessons\\LSGI522\\Practicals\\Practical1"
-    PATH = PARENT_DIR + "\\files\\"
-
+    PATH = PARENT_DIR + "\\tmp\\"
+    # Panzhiqing_CV.pdf files.pdf img-240618165302-001.pdf img-240618165332-001.pdf Visa MEEN-0012218-24.pdf Letter(1) MEEN-0012218-24.pdf
     # 需要合并的 PDF 文件列表
-    pdf_files = ["PanZhiQing24037665G.md.pdf","AD.pdf", "L.pdf"]
+    pdf_files = ["Panzhiqing_CV.pdf", "files.pdf", "img-240618165302-001.pdf", "img-240618165332-001.pdf", "Visa MEEN-0012218-24.pdf", "Letter(1) MEEN-0012218-24.pdf","7c.pdf"]
+
     pdf_files = [PATH + pdf for pdf in pdf_files]
 
     # 合并后的 PDF 文件
